010b955d4ff42621e3f7abacc7278a1396bf9a74
Alanjosh457 <alanjoshua515-email@gmail.com>
2025-05-08 17:39:13 +0530

    commit-3

--- front1/src/App.jsx
+++ front1/src/App.jsx
@@ -1,20 +1,15 @@
-// App.jsx or main.jsx
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Calender from './Calender';
 import Daily from './Daily';
 import Monthly from './Monthly';
 
-
-
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Calender />} />
-        <Route path="/daily" element={<Daily />} />
-        <Route path="/monthly" element={<Monthly />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Calender />} />
+      <Route path="/daily" element={<Daily />} />
+      <Route path="/monthly" element={<Monthly />} />
+    </Routes>
   );
 }
 
